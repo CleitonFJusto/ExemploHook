8d4aecae1abaa8c8c9d7e7443eb274297989c2c6
CleitonFJusto <cleitonjusto123@gmail.com>
2025-09-01 20:37:33 -0300

    tudo certo

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import { use, useState } from 'react'
 import './App.css'
 
 function App() {
-  
 
+
+
+  var sobreNome = "Justo";
   var nome = "Cleiton";
   const [name, setName] = useState("Valor inicial");
+  const [sobreName, setSobreName] = useState("Valor inicial");
 
 
   function mudarNome(event){
@@ -14,14 +17,23 @@ function App() {
     setName(event.target.value)
 
   }
+  function mudarSobrenome(event){
+    sobreNome = event.target.value;
+
+    setSobreName(event.target.value)
+  }
   return (
     <>
     <p>Inform seu nome</p>
     <input onChange={mudarNome}/>
+    <p>Informe seu sobrenome</p>
+    <input onChange={mudarSobrenome}/>
 
 
     <p>{nome}</p>
-    <p>Agora via useState : {name}</p>
+    <p>Seu nome via useState : {name}</p>
+    <p>Seu sobrenome via useState : {sobreName}</p>
+    
 
       
     </>
